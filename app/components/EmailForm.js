219592b5487fa41e88b6210e6a4b1d3ae176055f
omar <omarmc2@gmail.com>
2024-03-09 13:42:28 -0600

    last responsive changes

--- app/components/EmailForm.js
+++ app/components/EmailForm.js
@@ -158,179 +158,182 @@ const EmailForm = ({
   };
   console.log(showEmailForm);
   return (
-    <div className={"emailContainer"} hidden={showEmailForm}>
-      {error ? (
-        <Alert variant={"danger"}>
-          All fields are required, please fill in the missing ones.
-        </Alert>
-      ) : null}
-      <div className="prewritten-email-btn-container">
-        <p style={{fontWeight:"bold"}} className={"form-label"}> Dont have time to answer all these questions? </p> <Button className="prewritten-email-btn" variant="success" onClick={handleDefaultEmail} > Click here to send your MP a pre-written email. </Button>  
-      </div>
-      <Form name="fm-email" onSubmit={send} noValidate validated={validated}>
-        <div className={"formEmail"}>
-          <Col>
-            <Form.Group>
-              <Form.Label>{mainData.emailFormUserNameLabel}</Form.Label>
-              <Form.Control
-                plaintext="true"
-                id="username-emailform"
-                type="text"
-                placeholder={mainData.emailFormUserNamePlaceholder}
-                name="userName"
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="email">
-              <Form.Label>{mainData.emailFormUserLabel}</Form.Label>
-              <Form.Control
-                plaintext="true"
-                id="email-emailForm"
-                type="email"
-                placeholder={emailData.emailUser}
-                onChange={handleChange}
-                name="emailUser"
-                readOnly
-              />
-            </Form.Group>
-          </Col>
-        </div>
-        <Col>
-          <Form.Label>{mainData.emailFormInfoRepLabel}</Form.Label>
-        </Col>
-        <div className={"formEmail"}>
-          <Col>
-            <Form.Group>
-              <Form.Control
-                plaintext="true"
-                id="to-emailForm"
-                as={"input"}
-                readOnly
-                type="text"
-                placeholder={emailData.name}
-                name="nameTo"
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group>
-              <Form.Control
-                plaintext="true"
-                id="state-emailForm"
-                as={"input"}
-                readOnly
-                type="text"
-                placeholder={`${emailData.state}`}
-                name="state-city"
-              />
-            </Form.Group>
-          </Col>
-        </div>
-        <div className="formEmail">
-          <Col>
-            <Form.Group>
-              <Form.Label>{mainData.emailFormSubjectPlaceholder}</Form.Label>
-              <Form.Control
-                plaintext="true"
-                id="subject-emailform"
-                onChange={handleChange}
-                as="input"
-                type="text"
-                name="subject"
-                defaultValue={dataUser.subject}
-                required
-              />
-            </Form.Group>
-          </Col>
-        </div>
-        {hideAllQuestions === false ? (
-          mapingQuestions(dataQuestions)
-        ) : (
-          <Col className="questions">
-            <Form.Group>
-              <Form.Label>Message</Form.Label>
-              <Form.Control
-                plaintext="true"
-                id="message-emailform"
-                onChange={handleQuestions}
-                type="text-area"
-                as="textarea"
-                name={"question1"}
-                defaultValue={predefinedEmail}
-                rows={"5"}
-                required
-              />
-            </Form.Group>
-          </Col>
-        )}
-        <Loader
-          visible={showLoadSpin}
-          type="Puff"
-          color="#000000"
-          height={100}
-          width={100}
-          timeout={5000}
-        />
-        <Form.Group
-          style={{ textAlign: "justify" }}
-          className="field select-styles-form"
-          controlId="tickBox"
-        >
-          <Form.Check
-            name="tickBox"
-            onClick={handleTickBox}
-            required
-            label={
-              <p>
-                Would an increase in GP pricing due to this issue make you
-                consider your voting preference at the next election?
-              </p>
-            }
-          />
-        </Form.Group>
-        <div hidden={hideLastQuestion} className="questions">
-          <Col className="questions">
-            <Form.Group>
-              <Form.Label>
-                Tell your MP this sort of increase would make you consider
-                voting against them or their Party in the next election.
-              </Form.Label>
-              <Form.Control
-                plaintext="true"
-                id="message-emailform"
-                onChange={handleQuestions}
-                name="question1"
-              />
-            </Form.Group>
-          </Col>
-        </div>
-      </Form>
-      <div className={"container buttons-container-email-form"}>
-        <Button
-          id="backButton-emailform"
-          className={"button-email-form"}
-          variant={"dark"}
-          onClick={back}
-        >
-          {emailData.backButton
-            ? "please enter a back-button text on your dashboard"
-            : "Back"}
-        </Button>
+    <div className="email-form-container">
 
-        <Button
-          id="sendButton-emailform"
-          type={"submit"}
-          className={"button-email-form"}
-          variant={"dark"}
-          onClick={send}
-        >
-          {emailData.sendButton
-            ? "please enter a send-button text on your dashboard"
-            : "Send"}
-        </Button>
+      <div className={"emailContainer"} hidden={showEmailForm}>
+        {error ? (
+          <Alert variant={"danger"}>
+            All fields are required, please fill in the missing ones.
+          </Alert>
+        ) : null}
+        <div className="prewritten-email-btn-container">
+          <p style={{fontWeight:"bold"}} className={"form-label"}> Dont have time to answer all these questions? </p> <Button className="prewritten-email-btn" variant="success" onClick={handleDefaultEmail} > Click here to send your MP a pre-written email. </Button>  
+        </div>
+        <Form name="fm-email" onSubmit={send} noValidate validated={validated}>
+          <div className={"formEmail"}>
+            <Col>
+              <Form.Group>
+                <Form.Label>{mainData.emailFormUserNameLabel}</Form.Label>
+                <Form.Control
+                  plaintext="true"
+                  id="username-emailform"
+                  type="text"
+                  placeholder={mainData.emailFormUserNamePlaceholder}
+                  name="userName"
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="email">
+                <Form.Label>{mainData.emailFormUserLabel}</Form.Label>
+                <Form.Control
+                  plaintext="true"
+                  id="email-emailForm"
+                  type="email"
+                  placeholder={emailData.emailUser}
+                  onChange={handleChange}
+                  name="emailUser"
+                  readOnly
+                />
+              </Form.Group>
+            </Col>
+          </div>
+          <Col>
+            <Form.Label>{mainData.emailFormInfoRepLabel}</Form.Label>
+          </Col>
+          <div className={"formEmail"}>
+            <Col>
+              <Form.Group>
+                <Form.Control
+                  plaintext="true"
+                  id="to-emailForm"
+                  as={"input"}
+                  readOnly
+                  type="text"
+                  placeholder={emailData.name}
+                  name="nameTo"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.Control
+                  plaintext="true"
+                  id="state-emailForm"
+                  as={"input"}
+                  readOnly
+                  type="text"
+                  placeholder={`${emailData.state}`}
+                  name="state-city"
+                />
+              </Form.Group>
+            </Col>
+          </div>
+          <div className="formEmail">
+            <Col>
+              <Form.Group>
+                <Form.Label>{mainData.emailFormSubjectPlaceholder}</Form.Label>
+                <Form.Control
+                  plaintext="true"
+                  id="subject-emailform"
+                  onChange={handleChange}
+                  as="input"
+                  type="text"
+                  name="subject"
+                  defaultValue={dataUser.subject}
+                  required
+                />
+              </Form.Group>
+            </Col>
+          </div>
+          {hideAllQuestions === false ? (
+            mapingQuestions(dataQuestions)
+          ) : (
+            <Col className="questions">
+              <Form.Group>
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                  plaintext="true"
+                  id="message-emailform"
+                  onChange={handleQuestions}
+                  type="text-area"
+                  as="textarea"
+                  name={"question1"}
+                  defaultValue={predefinedEmail}
+                  rows={"5"}
+                  required
+                />
+              </Form.Group>
+            </Col>
+          )}
+          <Loader
+            visible={showLoadSpin}
+            type="Puff"
+            color="#000000"
+            height={100}
+            width={100}
+            timeout={5000}
+          />
+          <Form.Group
+            style={{ textAlign: "justify" }}
+            className="field select-styles-form"
+            controlId="tickBox"
+          >
+            <Form.Check
+              name="tickBox"
+              onClick={handleTickBox}
+              required
+              label={
+                <p>
+                  Would an increase in GP pricing due to this issue make you
+                  consider your voting preference at the next election?
+                </p>
+              }
+            />
+          </Form.Group>
+          <div hidden={hideLastQuestion} className="questions">
+            <Col className="questions">
+              <Form.Group>
+                <Form.Label>
+                  Tell your MP this sort of increase would make you consider
+                  voting against them or their Party in the next election.
+                </Form.Label>
+                <Form.Control
+                  plaintext="true"
+                  id="message-emailform"
+                  onChange={handleQuestions}
+                  name="question1"
+                />
+              </Form.Group>
+            </Col>
+          </div>
+        </Form>
+        <div className={"container buttons-container-email-form"}>
+          <Button
+            id="backButton-emailform"
+            className={"button-email-form"}
+            variant={"dark"}
+            onClick={back}
+          >
+            {emailData.backButton
+              ? "please enter a back-button text on your dashboard"
+              : "Back"}
+          </Button>
+
+          <Button
+            id="sendButton-emailform"
+            type={"submit"}
+            className={"button-email-form"}
+            variant={"dark"}
+            onClick={send}
+          >
+            {emailData.sendButton
+              ? "please enter a send-button text on your dashboard"
+              : "Send"}
+          </Button>
+        </div>
       </div>
     </div>
   );
